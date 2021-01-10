011f5adc64c91202a7c96dca18e4ab6ab0a06b61
walaam126 <walaamajeed12@gmail.com>
2021-01-10 13:00:35 +0300

    variables assignment solved

---
+++ story.js
@@ -0,0 +1,4 @@
+const username="walaa";
+let age=25;
+const hobby="photography";
+console.log(`Mr./Mrs. ${username}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
